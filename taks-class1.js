b7f3a10bce617c32a19e08babfe6a727dbd588d0
kartini <126692640+kartini27@users.noreply.github.com>
2023-03-15 23:26:50 -0700

    Add files via upload

---
+++ taks-class1.js
@@ -0,0 +1,56 @@
+// soal no 1
+class namaPerson {
+    name;
+    age;
+    gender;
+    height;
+    setName(name) {
+    this.name = name;
+    }
+    setAge(age) {
+    this.age = age;
+    }
+    setGender(gender){
+     this.gender = gender;   
+    }
+    setHeight(height) {
+    this.height = height;
+    }
+    getName(name){
+        name = kartini;
+    }
+    getAge(age){
+        age = 17;
+    }
+    getGender(gender){
+        gender = women;
+    }
+    getHeigth (height){
+        height = 164;
+    }
+
+}
+console.log('name');
+
+// soal no 2
+class student1{
+     nama = 'helveria';
+     nim = '2002020050';
+     daftarNilai= [70,80,90,95];
+     setNama(nama){
+        this.nama = nama;
+        setNim(nim){
+            this.nim = nim;
+        }
+        setDafatarNIlai(this.daftarNilai){
+            this.daftarNilai = daftarNilai;
+        }
+        const student1 = new class();
+        let student1= m.student1();
+        console.log(student1);
+        m.setstudent1('helvetia','20002004',[B,B+,A+,A])
+        dataStudent1 = m.getStudent1();
+        console.log (dataStudentl);
+        
+     }
+}
